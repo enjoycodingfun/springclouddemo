bdc04b9cc84ab1383e2f8eb3aa915e7e862e84c6
zz <627882647@qq.com>
2019-09-27 13:56:55 +0800

    Initial commit

---
+++ provider/src/main/java/com/motherbuy/ProviderApplication.java
@@ -0,0 +1,12 @@
+package com.motherbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
+
